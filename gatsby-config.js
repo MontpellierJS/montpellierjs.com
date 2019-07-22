5f5ce4e02fbe0f6c581837ad701719c9e2194206
Loïc <mimisteus@gmail.com>
2019-07-22 13:17:31 +0200

    Initial commit with Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `MontpellierJS`,
+    description: ``,
+    author: ``,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MontpellierJS`,
+        start_url: `/`,
+      },
+    },
+  ],
+}
