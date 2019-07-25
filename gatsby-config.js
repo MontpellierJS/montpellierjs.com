0cbf72cbce4cbd4bc79a82ecaaaf36aa7995d651
Loïc <mimisteus@gmail.com>
2019-07-25 13:47:25 +0200

    Added contentful support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `MontpellierJS`,
@@ -5,6 +8,13 @@ module.exports = {
     author: ``,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_TOKEN,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
