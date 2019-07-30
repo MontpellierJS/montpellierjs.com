6e09f94889730bd16e5545efa88f4af56ccbcdf6
Loïc <mimisteus@gmail.com>
2019-07-30 14:13:42 +0200

    Layout update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'gatsby'
 
 const Header = () => {
   return (
     <header>
-      <Navbar className="justify-content-between">
-        <Navbar.Brand>Brand link</Navbar.Brand>
-        <Nav className="ml-auto">
-          <Link activeClassName="active" className="nav-link" to="/">
-            Home
-          </Link>
-          <Link activeClassName="active" className="nav-link" to="/meetups">
-            Meetups
-          </Link>
-          <Link activeClassName="active" className="nav-link" to="/team">
-            Team
-          </Link>
-          <Link activeClassName="active" className="nav-link" to="/contact">
-            Contact
-          </Link>
-        </Nav>
-      </Navbar>
+      <Container>
+        <Navbar className="justify-content-between">
+          <Navbar.Brand>Brand link</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Link activeClassName="active" className="nav-link" to="/">
+              Home
+            </Link>
+            <Link activeClassName="active" className="nav-link" to="/meetups">
+              Meetups
+            </Link>
+            <Link activeClassName="active" className="nav-link" to="/team">
+              Team
+            </Link>
+            <Link activeClassName="active" className="nav-link" to="/contact">
+              Contact
+            </Link>
+          </Nav>
+        </Navbar>
+      </Container>
     </header>
   )
 }
--- src/components/MjsLayout.js
+++ src/components/MjsLayout.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Container } from 'react-bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
 const MjsLayout = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <Container>{children}</Container>
       <Footer />
     </>
   )
